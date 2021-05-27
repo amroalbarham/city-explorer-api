2f30b549f1e7fe71667edae9400dade090b5ca01
amroalbarham <amroalbarham@gmail.com>
2021-05-27 16:04:24 +0300

    finish back end lab9+10

---
+++ modules/weather.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+
+module.exports = weatherhandler;
+
+
+class ForCast {
+    constructor(item) {
+        this.date = item.valid_date;
+        this.description = `low of ${item.min_temp}, hight of${item.max_temp} with ${item.weather.description}`;
+    }
+}
+let inmemory = {};
+
+function weatherhandler(req, res) {
+    let key = process.env.WEATHER_KEY;
+
+    let city = req.query.searchQuery;
+    // let lat = req.query.lat;
+    // let long = req.query.long;
+    // let e = true;
+    if (inmemory[city] !== undefined) {
+        console.log('get the data from the weatherhandler Memory');
+        res.send(inmemory[city]);
+    } else {
+        console.log('get the data from the weatherhandler API');
+
+        let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`;
+        // let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=amman&key=989b3581ea7b480981843ed67b5214a6`;
+
+        axios
+            .get(url)
+            .then(found => {
+                const weatherArr = found.data.data.map(value => {
+                    return new ForCast(value);
+                })
+                inmemory[city] = weatherArr;
+                res.send(weatherArr);
+            })
+
+
+            .catch(error => {
+
+                res.status(500).send(`Sorry, cant find that ${error}`);
+            })
+    }
+
+
+}
--- server.js
+++ server.js
@@ -16,91 +16,94 @@ const PORT = process.env.PORT;
 server.listen(PORT, () => {
     console.log(`server lisin ${PORT}`);
 })
-
+const moviehandler=require('./modules/movie');
 server.get('/movie', moviehandler);
 
+const weatherhandler=require('./modules/weather');
 server.get('/weather', weatherhandler);
 
-class ForCast {
-    constructor(item) {
-        this.date = item.valid_date;
-        this.description = `low of ${item.min_temp}, hight of${item.max_temp} with ${item.weather.description}`;
-    }
-}
 
-function weatherhandler(req, res) {
-    let key = process.env.WEATHER_KEY;
+///////////////////////////////////////////////////////////////////////////////////////////
+// class ForCast {
+//     constructor(item) {
+//         this.date = item.valid_date;
+//         this.description = `low of ${item.min_temp}, hight of${item.max_temp} with ${item.weather.description}`;
+//     }
+// }
 
-    let city = req.query.searchQuery;
-    // let lat = req.query.lat;
-    // let long = req.query.long;
-    // let e = true;
+// function weatherhandler(req, res) {
+//     let key = process.env.WEATHER_KEY;
 
-    let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`;
-    // let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=amman&key=989b3581ea7b480981843ed67b5214a6`;
+//     let city = req.query.searchQuery;
+//     // let lat = req.query.lat;
+//     // let long = req.query.long;
+//     // let e = true;
 
-    axios
-        .get(url)
-        .then(found => {
-            const weatherArr = found.data.data.map(value => {
-                return new ForCast(value);
-            })
-            res.send(weatherArr);
-        })
+//     let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`;
+//     // let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=amman&key=989b3581ea7b480981843ed67b5214a6`;
+
+//     axios
+//         .get(url)
+//         .then(found => {
+//             const weatherArr = found.data.data.map(value => {
+//                 return new ForCast(value);
+//             })
+//             res.send(weatherArr);
+//         })
 
 
-        .catch(error => {
+//         .catch(error => {
 
-            res.status(500).send(`Sorry, cant find that ${error}`);
-        })
+//             res.status(500).send(`Sorry, cant find that ${error}`);
+//         })
         
-}
+// }
 
 
+//////////////////////////////////////////////////////////////////////////////////////
 
+// class Formovie {
+//     constructor(item) {
+//         this.title = item.original_title;
+//         this.overview = item.overview;
+//         this.avgVotes = item.vote_average;
+//         this.totalVotes= item.vote_count;
+//         this.imagePath =`https://image.tmdb.org/t/p/w500${item.poster_path}`;
+//         this.popularity = item.popularity;
+//         this.releaseDate = item.release_date;
+//     }
+// }
 
-class Formovie {
-    constructor(item) {
-        this.title = item.original_title;
-        this.overview = item.overview;
-        this.avgVotes = item.vote_average;
-        this.totalVotes= item.vote_count;
-        this.imagePath =`https://image.tmdb.org/t/p/w500${item.poster_path}`;
-        this.popularity = item.popularity;
-        this.releaseDate = item.release_date;
-    }
-}
-
-function moviehandler(req, res) {
+// function moviehandler(req, res) {
     
-    let key1 = process.env.MOVIE_API_KEY;
-    let city = req.query.searchQuery;
-    // let lat = req.query.lat;
-    // let long = req.query.long;
-    // let e = true;
+//     let key1 = process.env.MOVIE_API_KEY;
+//     let city = req.query.searchQuery;
+//     // let lat = req.query.lat;
+//     // let long = req.query.long;
+//     // let e = true;
 
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${key1}&query=${city}`;
-    // let url = `https://api.themoviedb.org/3/search/movie?api_key=e36cdf3c2b2019fad98b1fe0bb3b9f1c&query=amman`;
+//     let url = `https://api.themoviedb.org/3/search/movie?api_key=${key1}&query=${city}`;
+//     // let url = `https://api.themoviedb.org/3/search/movie?api_key=e36cdf3c2b2019fad98b1fe0bb3b9f1c&query=amman`;
 
-    axios.get(url)
-        .then(found => {
-            const movieArr = found.data.results.map(elemnt => {
-                return new Formovie(elemnt);
-            })
-            // console.log(movieArr);
-            res.send(movieArr);
-        })
+//     axios.get(url)
+//         .then(found => {
+//             const movieArr = found.data.results.map(elemnt => {
+//                 return new Formovie(elemnt);
+//             })
+//             // console.log(movieArr);
+//             res.send(movieArr);
+//         })
 
 
-        .catch(error => {
-            console.log(error);
-            res.status(500).send(`Sorry, cant find that so please don't write irbid ${error}`);
+//         .catch(error => {
+//             console.log(error);
+//             res.status(500).send(`Sorry, cant find that so please don't write irbid ${error}`);
             
-        })
+//         })
         
-}
+// }
 
-
+///////////////////////////////////////////////////////////////////////////////////////
 
 
 
